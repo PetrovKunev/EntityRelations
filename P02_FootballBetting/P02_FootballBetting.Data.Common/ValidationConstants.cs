0825360e14589f70fa1db05f7a92a472e768c1e7
Yavor Petrov <petrovkunev@gmail.com>
2024-06-29 10:40:11 +0300

    Folder_Name

--- P02_FootballBetting/P02_FootballBetting.Data.Common/ValidationConstants.cs
+++ P02_FootballBetting/P02_FootballBetting.Data.Common/ValidationConstants.cs
@@ -5,5 +5,8 @@ namespace P02_FootballBetting.Data.Common
     public class ValidationConstants
     {
         public const int CountryNameMaxLength = 50;
+
+
+        public const int TownNameMaxLength = 50;
     }
 }
