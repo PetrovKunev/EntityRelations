374b0061ea5a94cb4459a101ab8ca0b533efa671
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2024-07-03 19:17:09 +0300

    2,3,4

--- BookShop/BookShop.Data/Configuration.cs
+++ BookShop/BookShop.Data/Configuration.cs
@@ -3,6 +3,6 @@
     internal class Configuration
     {
         internal static string ConnectionString
-            => "Server=.;Database=BookShop;Integrated Security=True;";
+            => "Server=MENTAT\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
     }
 }
--- BookShop/BookShop/StartUp.cs
+++ BookShop/BookShop/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
 
@@ -8,7 +9,54 @@
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
+
+            //Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+            //Console.WriteLine(GetGoldenBooks(db));
+            Console.WriteLine(GetBooksByPrice(db));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            command = command.ToLower();
+
+            
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
+            {
+                return "Invalid age restriction.";
+            }
+
+            
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+            
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => $"{b.Title} - ${b.Price:F2}")
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
         }
     }
 }
